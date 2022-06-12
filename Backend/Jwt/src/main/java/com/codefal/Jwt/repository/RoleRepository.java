1fe515c578f4a0832f03fa3518429cee7eb130a4
Akshay V Anil <akshayvanil117@gmail.com>
2022-06-12 20:53:02 +0530

    Add : Configured Repository

--- Backend/Jwt/src/main/java/com/codefal/Jwt/repository/RoleRepository.java
+++ Backend/Jwt/src/main/java/com/codefal/Jwt/repository/RoleRepository.java
@@ -4,7 +4,9 @@ import com.codefal.Jwt.models.ERole;
 import com.codefal.Jwt.models.Role;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
   Optional<Role> findByName(ERole name);
 }
